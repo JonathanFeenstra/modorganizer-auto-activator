bb215acd2966d5729f4870b68738e8e2b742d7f6
Jonathan Feenstra <26406078+JonathanFeenstra@users.noreply.github.com>
2023-07-11 20:27:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Auto-Activator
+[Mod Organizer 2](https://github.com/ModOrganizer2/modorganizer) plugin to automatically activate mods on installation.
+
+This is now possible thanks to a [bugfix](https://github.com/ModOrganizer2/modorganizer/pull/1837) in version 2.5.0 of Mod Organizer, which has not yet been released at the time of writing. As such, this plugin currently only works with pre-release builds of Mod Organizer v2.5.
---
+++ auto_activator.py
@@ -0,0 +1,47 @@
+"""
+Auto-Activator Plugin for Mod Organizer 2
+
+Copyright (C) 2023 Jonathan Feenstra
+"""
+import mobase
+from PyQt6.QtCore import qCritical
+from PyQt6.QtWidgets import QApplication
+
+
+class AutoActivator(mobase.IPlugin):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        self.__organizer = organizer
+
+        if not self.__organizer.modList().onModInstalled(self.__onModInstalled):
+            qCritical("Failed to install onModInstalled callback.")
+            return False
+
+        return True
+
+    def name(self) -> str:
+        return "Auto-Activator"
+
+    def author(self) -> str:
+        return "Jonathan Feenstra"
+
+    def description(self) -> str:
+        return self.__tr("Automatically activate mods after installing.")
+
+    def version(self) -> mobase.VersionInfo:
+        return mobase.VersionInfo(1, 0, 0, mobase.ReleaseType.FINAL)
+
+    def settings(self) -> list[mobase.PluginSetting]:
+        return []
+
+    def __onModInstalled(self, mod: mobase.IModInterface) -> None:
+        self.__organizer.modList().setActive(name=mod.name(), active=True)
+
+    def __tr(self, txt: str) -> str:
+        return QApplication.translate("AutoActivator", txt)
+
+
+def createPlugin() -> mobase.IPlugin:
+    return AutoActivator()
